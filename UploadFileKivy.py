2b71e90cff58b341ae57b06d82b24e3a3da0e8b6
noorwa1 <75200572+noorwa1@users.noreply.github.com>
2021-01-06 17:26:25 +0200

    Update UploadFileKivy.py

--- UploadFileKivy.py
+++ UploadFileKivy.py
@@ -4,22 +4,22 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 class Filechooser(BoxLayout):
     def select(self, *args):
+        firebaseconfig = {'apiKey': "AIzaSyBWUU7eIQD13XhdWc07ZcmbtOPwu4kkYbM",
+                          'authDomain': "file-uploading-2e4d3.firebaseapp.com",
+                          'projectId': "file-uploading-2e4d3",
+                          'storageBucket': "file-uploading-2e4d3.appspot.com",
+                          'messagingSenderId': "420295748286",
+                          'appId': "1:420295748286:web:cb3f00a77a022b4fedccef",
+                          'measurementId': "G-DHDSH9ST86"}
+        firebase = pyrebase.initialize_app(firebaseconfig)
+        storage = firebase.storage()
+        cloud_file_name = "files/Uploaded"
+        filename = args[1]
+        storage.child(cloud_file_name).put(filename)
         try:
 
             self.label.text = args[1][0]
-            if args :
-                firebaseconfig = {'apiKey': "AIzaSyBWUU7eIQD13XhdWc07ZcmbtOPwu4kkYbM",
-                                  'authDomain': "file-uploading-2e4d3.firebaseapp.com",
-                                  'projectId': "file-uploading-2e4d3",
-                                  'storageBucket': "file-uploading-2e4d3.appspot.com",
-                                  'messagingSenderId': "420295748286",
-                                  'appId': "1:420295748286:web:cb3f00a77a022b4fedccef",
-                                  'measurementId': "G-DHDSH9ST86"}
-                firebase = pyrebase.initialize_app(firebaseconfig)
-                storage = firebase.storage()
-                cloud_file_name = "files/Uploaded"
-                filename = args
-                storage.child(cloud_file_name).put(filename)
+
 
         except:
             pass
